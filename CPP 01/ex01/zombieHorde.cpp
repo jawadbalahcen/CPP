911ff151d60416c803154ed3f96f16d43868e002
Jawad Balahcen <jbalahce@e1r9p3.1337.ma>
2023-07-22 16:54:11 +0100

    cpp01 update

--- CPP 01/ex01/zombieHorde.cpp
+++ CPP 01/ex01/zombieHorde.cpp
@@ -6,15 +6,16 @@
 /*   By: jbalahce <jbalahce@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/27 17:51:39 by jbalahce          #+#    #+#             */
-/*   Updated: 2023/05/28 16:10:36 by jbalahce         ###   ########.fr       */
+/*   Updated: 2023/07/22 16:53:06 by jbalahce         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Zombie.hpp"
 
-
 Zombie*	zombieHorde(int N, std::string name)
 {
+	if (N <= 0)
+		return (NULL);
 	Zombie *zombies = new Zombie[N];
 
 	for (int i = 0; i < N; i++){
