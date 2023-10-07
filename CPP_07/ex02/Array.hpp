35dc212e4112ecc71ada7e6405812a378e37ad73
Jawad Balahcen <jbalahce@c3r6p5.1337.ma>
2023-10-07 16:39:26 +0100

    cpp07 finished

--- CPP_07/ex02/Array.hpp
+++ CPP_07/ex02/Array.hpp
@@ -6,24 +6,64 @@
 /*   By: jbalahce <jbalahce@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/06 15:47:36 by jbalahce          #+#    #+#             */
-/*   Updated: 2023/10/06 17:10:49 by jbalahce         ###   ########.fr       */
+/*   Updated: 2023/10/07 16:31:25 by jbalahce         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #pragma once
 
 #include <iostream>
+#include <stdexcept>
 
 template <typename T>
-void iter(T* array, size_t length, void (*func)(T &))
-{
-    for (size_t i = 0; i < length; ++i)
-        func(array[i]);
-}
+class Array {
+    private:
+        T* data;
+        unsigned int length;
 
-template <typename T>
-void print(T &a)
-{
-    std::cout << a << std::endl;
-}
+    public:
+        Array() : data(NULL), length(0) {}
+
+        Array(unsigned int n) : length(n)
+        {
+            data = new T[length];
+            for (unsigned int i = 0; i < length; ++i)
+                data[i] = T();
+        }
+
+        Array(const Array& other) : length(other.length)
+        {
+            data = new T[length];
+            for (unsigned int i = 0; i < length; ++i)
+                data[i] = other.data[i];
+        }
+
+        Array& operator=(const Array& other)
+        {
+            if (this != &other)
+            {
+                delete[] data;
+                length = other.length;
+                data = new T[length];
+                for (unsigned int i = 0; i < length; ++i)
+                    data[i] = other.data[i];
+            }
+            return *this;
+        }
+
+        ~Array() {
+            delete[] data;
+        }
+
+        T& operator[](unsigned int index) {
+            if (index >= length)
+                throw std::out_of_range("Index out of bounds");
+            return data[index];
+        }
+
+        unsigned int size() const {
+            return length;
+        }
+};
+
 
--- CPP_07/ex02/main.cpp
+++ CPP_07/ex02/main.cpp
@@ -6,36 +6,43 @@
 /*   By: jbalahce <jbalahce@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/06 15:55:42 by jbalahce          #+#    #+#             */
-/*   Updated: 2023/10/06 16:06:41 by jbalahce         ###   ########.fr       */
+/*   Updated: 2023/10/07 16:31:31 by jbalahce         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "iter.hpp"
+#include "Array.hpp"
 
 int main()
 {
-    std::cout << "Testing with Float" << std::endl;
-    float farr[] = {0.f, 5.f, 6.1f, 12.f};
-    iter(farr, 4, print);
-    std::cout << "-----------------------------------" << std::endl;
+    Array<int> arry1(8);
+    Array<int> arry2(4);
 
-    std::cout << "Testing with Double" << std::endl;
-    double darr[] = {0.01, 5.002, 7.1002, 12.002};
-    iter(darr, 4, print);
-    std::cout << "-----------------------------------" << std::endl;
+    for (unsigned int i = 0; i < arry1.size(); i++)
+        arry1[i] = i * 3;
+        
+    std::cout << "----------ARRY1 ELEMENTS----------\n";
+    for (unsigned int i = 0; i < arry1.size(); i++)
+        std::cout<< "At index "<< i <<" : " << arry1[i] << std::endl;
+    std::cout << "----------------------------------\n";
+    
+    arry2 = arry1;
+    
+    std::cout << "----------ARRY2 ELEMENTS----------\n";
+    for (unsigned int i = 0; i < arry2.size(); i++)
+        std::cout<< "At index "<< i <<" : " << arry2[i] << std::endl;
+    std::cout << "----------------------------------\n";
 
-    std::cout << "Testing with Int" << std::endl;
-    int iarr[] = {0, 62, 65, 127};
-    iter(iarr, 4, print);
-    std::cout << "-----------------------------------" << std::endl;
+    std::cout << "ARRY1 SIZE: "<< arry1.size() << std::endl;
+    std::cout << "ARRY2 SIZE: "<< arry2.size() << std::endl;
 
-    std::cout << "Testing with char" << std::endl;
-    char carr[] = {'a', 'b', 'c', 'd'};
-    iter(carr, 4, print);
-    std::cout << "-----------------------------------" << std::endl;
+    std::cout << "----------EXCEPTION----------\n";
+    try
+    {
+        arry1[-1];
+    }catch (std::exception &e)
+    {
+        std::cout << "Error: "<< e.what() << std::endl;
+    }
+    std::cout << "------------------------------\n";
 
-    std::cout << "Testing with string" << std::endl;
-    std::string sarr[] = {"s1", "s222", "str", "chain"};
-    iter(sarr, 4, print);
-    std::cout << "-----------------------------------" << std::endl;
 }
